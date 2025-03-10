088bc3ff0171685cb114d5df79fe69eed660ab32
Alejandra <86182320+alegjs044@users.noreply.github.com>
2025-03-09 21:47:23 -0600

    add changes

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 
 // Configuración de conexión a la base de datos con SSL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "bibnsazm4qab9oq1jxct-mysql.services.clever-cloud.com",
+    host: process.env.DB_HOST || "blaynrrtogkqgpd39b0q-mysql.services.clever-cloud.com",
     user: process.env.DB_USER || "ubpn9ejvew4qdfxt",
     password: process.env.DB_PASSWORD || "mM4h7WbPcthQaUqknkBO",
-    database: process.env.DB_NAME || "bibnsazm4qab9oq1jxct",
-    port: process.env.DB_PORT || 3306, 
+    database: process.env.DB_NAME || "blaynrrtogkqgpd39b0q",
+    port: process.env.DB_PORT || 20220, 
     ssl: { rejectUnauthorized: false } 
 });
 
